e211b4e353288d950be3baa13cd1b8f472a6e272
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 07:34:14 +0700

    test

---
+++ Modul5_103022400016/SimpleDataBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class SimpleDataBase
+{
+	public SimpleDataBase()
+	{
+		List<T> StoreData;
+		List<DateTime> inputDates;
+
+		public SimpleDataBase()
+		{
+			this.StoreData = new List<T>;
+			this.inputDates = new List<DateTime>();
+		}
+
+		public void AddNewData(T dbbaru)
+		{
+			this.StoreData.Add(dbbaru);
+			this.inputDates.Add(System.DateTime.Now);
+		}
+		
+		public 
+
+    }
+}
