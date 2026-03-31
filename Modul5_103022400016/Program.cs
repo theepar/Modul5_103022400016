dbcd9147dceadd3a0cf4142587a893eae667c209
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 07:21:46 +0700

    fix

--- Modul5_103022400016/Program.cs
+++ Modul5_103022400016/Program.cs
@@ -2,8 +2,23 @@
 
 namespace PemrosesData
 {
-    public void DapatkanNilaiTerbesar()
-    {
+    public class PemrosesData
+    { 
+        public T DapatkanNilaiTerbesar<T>(T x,T y,T z)
+        {
+            dynamic a = x;
+            dynamic b = y;
+            dynamic c = z;
+            dynamic temp = a;
 
-    }
+            if (temp < b)
+            {
+                temp = b;
+            }
+            if (temp > c) {
+                temp = c;
+            }
+            return temp;
+        }
+     }
 }
